a5e49af7cd467994bf9b63f74e03b7f324511fd3
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-02 16:27:28 +0200

    fix finali

--- js/script.js
+++ js/script.js
@@ -5,17 +5,21 @@ for(let i=1; i<=100; i++) {
         element.classList.add("item" ,"fizzbuzz");
         element.innerText = "FizzBuzz";
         list.append(element);
+        console.log("FizzBuzz");
     } else if (i % 3 === 0){
         element.classList.add("item" , "fizz");
         element.innerText = "Fizz";
         list.append(element);
+        console.log("Fizz");
     } else if (i % 5 === 0) {
         element.classList.add("item" , "buzz");
         element.innerText = "Buzz";
         list.append(element);
+        console.log("Buzz");
     } else {
         element.classList.add("item");
         element.innerText = i;
         list.append(element);
+        console.log(i);
     }
 }
